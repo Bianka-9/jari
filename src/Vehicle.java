54052daffd4406c1edb74edc3895cd88905c0751
diak <you@example.com>
2025-02-25 10:47:09 +0100

    Vehicle

---
+++ src/Vehicle.java
@@ -0,0 +1,16 @@
+import java.util.Date;
+
+public class Vehicle {
+    String license_plate;
+    Integer brand;
+    Integer year;
+    String fuel;
+    Double price;
+    String ac;
+    Date hun_license;
+
+    
+}
+/*Az alábbi állományban járműveik adatait láthatjuk,
+a következő oszlopok szerint: rendszám, márka, évjárat, üzemanyag, eladási ár, 
+klíma, magyar forgalmi érvényessége. */
