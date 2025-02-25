8e64ab04a860a24e8d7e840878362deff7d3df7f
diak <you@example.com>
2025-02-25 11:03:33 +0100

    hellö

--- src/SumData.java
+++ src/SumData.java
@@ -5,5 +5,5 @@ public class SumData {
     vehicleList.add(new Vehicle("ABC-123","Honda",
      2018,"benzin",450.0,1, new LocalDate.parse("2026-04-07")));
 
-     vehicleList.add(vehicle)
+     vehicleList.add(vehicle);
 }
